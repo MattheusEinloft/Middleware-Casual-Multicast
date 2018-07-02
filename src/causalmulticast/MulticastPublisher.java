6cbaa4e89403fd80bd4f22262d8446229399c945
Mattheus <mattheuseinloft@gmail.com>
2018-07-02 16:15:47 -0300

    Método "mcsend" ajustado

--- src/causalmulticast/MulticastPublisher.java
+++ src/causalmulticast/MulticastPublisher.java
@@ -4,28 +4,32 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketException;
 
 /**
  *
  * @author Mattheus
  */
 public class MulticastPublisher {
-    private DatagramSocket socket;
+    private static DatagramSocket socket;
   //  private InetAddress group;
     private byte[] buf;
+
+    public MulticastPublisher(DatagramSocket socket) throws SocketException {
+        this.socket = socket;
+    }
     
     public void mcsend(String multicastMessage, InetAddress group) throws IOException {
-        socket = new DatagramSocket();
     //    group = InetAddress.getByName("225.0.0.0");
         buf = multicastMessage.getBytes();
         
         DatagramPacket packet = new DatagramPacket(buf, buf.length, group, 4446);
         socket.send(packet);
-        socket.close();
     }
     
     public static void main(String[] args) throws IOException{
-        MulticastPublisher publicador = new MulticastPublisher();
+        MulticastPublisher publicador = new MulticastPublisher(new DatagramSocket());
         publicador.mcsend("Hello", InetAddress.getByName("225.0.0.0"));
+        socket.close();
     }
 }
